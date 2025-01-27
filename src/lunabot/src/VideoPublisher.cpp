4fec5251bfed8b7eaef310b16fc6d1c56f67f04f
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2025-01-27 14:49:25 -0800

    fix videopub

--- src/lunabot/src/VideoPublisher.cpp
+++ src/lunabot/src/VideoPublisher.cpp
@@ -11,21 +11,44 @@ class VideoPublisher : public rclcpp::Node {
   public:
     VideoPublisher() : Node('camera_node') {
       publisher_ = this->create_publisher<sensor_msgs::msg::Image>("raw_image", 20);
+
+      video_cap_.open(0);
+      if (!video_cap_.isOpened()) {
+        RCLCPP_ERROR(this->get_logger(), "Video camera failed to open. Exiting...");
+        return;
+      }
+
       timer_ = this->create_wall_timer(
-        std::chrono::milliseconds(100),
+        std::chrono::milliseconds(33),
         std::bind(&VideoPublisher::timer_callback, this);
       );
     }
   private:
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+  cv::VideoCapture video_cap_;
 
   void timer_callback() {
+    cv::Mat frame;
+
+    if (video_cap_.read(frame)) {
+      sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(
+        std_msgs::msg::Header(),
+        "bgr8",
+        frame
+      ).toImageMsg();
+
+      msg->header.stamp = this->now();
+      msg->header.frame_id = "raw_image";
+      publisher_->publish(*msg);
+    }
   }
 };
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
+
+  rclcpp::spin(std::make_shared<VideoPublisher>());
 
   rclcpp::shutdown();
   return 0;
