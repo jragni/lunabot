bb655d1c061ec63a00b351721703a780ac327462
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2025-01-27 17:09:39 -0800

    fix launch descriptions

--- src/lunabot/launch/video_publisher.launch.py
+++ src/lunabot/launch/video_publisher.launch.py
@@ -3,10 +3,10 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription(
+    return LaunchDescription([
         Node(
             package="lunabot",
             executable="camera_node",
             output="screen"
         )
-    )
+    ])
