2a64faef0058c30ddacba0edd5e8299f889ede69
Jhensen Ray Agni <jhensenrayagni@gmaill.com>
2025-01-30 18:32:01 -0800

    update

--- src/lunabot/setup.py
+++ src/lunabot/setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='my_mixed_package',
+    name='lunabot',
     version='0.0.1',
-    packages=['my_mixed_package'],
+    packages=['lunabot'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + 'my_mixed_package']),
-        ('share/' + 'my_mixed_package', ['package.xml']),
+        ('share/' + 'lunabot', ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    author='Your Name',
-    author_email='your@email.com',
-    maintainer='Your Name',
-    maintainer_email='your@email.com',
+    author='Jhensen Ray Agni',
+    author_email='jhensenrayagni@gmail.com',
+    maintainer='Jhensen Ray Agni',
+    maintainer_email='jhensenrayagni@gmail.com',
     description='A mixed C++ and Python ROS2 package',
     license='Apache License 2.0',
     tests_require=['pytest'],
